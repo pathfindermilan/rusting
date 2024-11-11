922ee4d15bc0812ce6c1fcc400987a8709e46b10
pathfinder.milan <pathfinder.milan@protonmail.com>
2024-11-11 02:46:16 +0100

    [Section2] Finish with the simple deck app

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,40 @@
+use rand::{thread_rng, seq::SliceRandom};
+
 #[derive(Debug)]
 struct Deck {
     cards: Vec<String>,
 }
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Diamonds"];
+        let values = ["Ace", "Two", "Three"];
 
-fn main() {
-    let suits = ["Hearts", "Spades", "Diamonds"];
-    let values = ["Ace", "Two", "Three"];
+        let mut cards = vec![];
 
-    let mut cards = vec![];
-
-    for suit in suits {
-        for value in values {
-            let card = format!("{} of {}", value, suit);
-            cards.push(card);
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
         }
+
+        Deck {cards}
+    }
+    fn shuffle(&mut self) {
+        let mut rng = thread_rng();
+        self.cards.shuffle(&mut rng);
     }
 
-    let deck = Deck { cards };
+    fn deal(&mut self, num_cards: usize) -> Vec<String>{
+        self.cards.split_off(
+            self.cards.len() - num_cards
+        )
+    }
+}
 
-    println!("Here is your deck: {:#?}", deck);
+fn main() {
+    let mut deck = Deck::new();
+    let cards = deck.deal(3);
+
+    println!("Here is your deck: {:#?}", cards);
 }
