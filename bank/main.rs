af0332aa7cd7ee7e9e78f2a332c6b51f54c24f49
pathfinder.milan <pathfinder.milan@protonmail.com>
2024-11-14 00:49:38 +0100

    [Section3] WrapUp of the bank project

--- bank/main.rs
+++ bank/main.rs
@@ -12,6 +12,18 @@ impl Account {
             balance: 0,
         }
     }
+    fn deposit(&mut self, amount: i32) -> i32 {
+        self.balance += amount;
+        self.balance
+    }
+    fn withdraw(&mut self, amount: i32) -> i32 {
+        self.balance -= amount;
+        self.balance
+    }
+
+    fn summary(&self) -> String {
+        format!("{} has a balance {}", self.holder, self.balance)
+    }
 }
 
 #[derive(Debug)]
@@ -22,20 +34,32 @@ impl Bank {
     fn new() -> Self {
         Bank { accounts: vec![] }
     }
-}
+    fn add_account(&mut self, account: Account) {
+        self.accounts.push(account);
+    }
+    fn total_balance(&self) -> i32 {
+        self.accounts.iter().map(|account| account.balance).sum()
+    }
 
-fn add_account(bank: &mut Bank, account:  Account) {
-    bank.accounts.push(account);
+    fn summary(&self) -> Vec<String> {
+        self.accounts
+            .iter()
+            .map(|account| account.summary())
+            .collect::<Vec<String>>()
+    }
 }
 
 fn main() {
     let mut bank = Bank::new();
-    let account = Account::new(
+    let mut account = Account::new(
         1,
         String::from("me")
     );
 
-    add_account(&mut bank, account);
+    account.deposit(500);
+    account.withdraw(250);
 
-    println!("{:#?}", bank);
+    bank.add_account(account);
+    println!("{:#?}", bank.summary());
+    println!("{}", bank.total_balance());
 }
