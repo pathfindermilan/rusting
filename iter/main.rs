66b2afd8c158f5f06baa116140b6ec69d0bd9220
pathfindermilan <pathfinder.milan@protonmail.com>
2024-11-23 12:33:03 +0100

    [Section8] Deep dive into iterators

---
+++ iter/main.rs
@@ -0,0 +1,69 @@
+fn print_element(elements: &[String]) {
+    elements
+        .iter()
+        .map(|el| format!("{} {}", el, el))
+        .for_each(|el| println!("{}", el));
+}
+
+fn shorten_strings(elements: &mut [String]) {
+    elements
+        .iter_mut()
+        .for_each(|el| el.truncate(1))
+}
+
+fn to_uppercase(elements: &[String]) -> Vec<String> {
+    elements
+        .iter()
+        .map(|el| el.to_uppercase())
+        .collect::<Vec<String>>()
+}
+
+fn move_elements(vec_a: Vec<String>, vec_b: &mut Vec<String>) {
+    vec_a.into_iter().for_each(|el| vec_b.push(el))
+}
+
+fn explode(elements: &[String]) -> Vec<Vec<String>> {
+    elements
+        .iter()
+        .map(|el| el.chars().map(|c| c.to_string()).collect())
+        .collect()
+}
+
+fn find_color_or(elements: &[String], search: &str, fallback: &str) -> String {
+    elements
+        .iter()
+        .find(|el| el.contains(search))
+        .map_or(
+            String::from(fallback),
+            |el| el.to_string()
+        )
+}
+fn main () {
+    let colors = vec![
+        String::from("red"),
+        String::from("green"),
+        String::from("blue"),
+    ];
+
+    // print_element(&colors[1..3]);
+    // shorten_strings(&mut colors[..]);
+    // println!("{:#?}", colors);
+
+    // let uppercased = to_uppercase(&colors);
+    // println!("{:#?}", uppercased);
+
+    // let mut destination = vec![];
+    // move_elements(colors, &mut destination);
+    // println!("{:#?}", destination);
+
+    // let exploded = explode(&colors);
+    // println!("{:#?}", exploded);
+
+    let found_color = find_color_or(
+        &colors,
+        "gr",
+        "orange"
+    );
+
+    println!("{:#?}", found_color);
+}
